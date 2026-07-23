992ca30a0d254f5e831b9854d0ab199d62d1d222
KevinRabbe <rabbekevin@icloud.com>
2026-07-23 23:52:54 +0200

    build: target stable Paper 26.1.2

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":common"))
-    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
 
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
