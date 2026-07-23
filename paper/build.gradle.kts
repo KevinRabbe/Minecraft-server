c31d72d1c22f14ee2e5cc18c02bf09dd0b2fb350
KevinRabbe <rabbekevin@icloud.com>
2026-07-23 23:50:58 +0200

    build: package common code into Paper plugin

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,7 +1,23 @@
+plugins {
+    id("com.gradleup.shadow") version "9.6.1"
+}
+
 dependencies {
     implementation(project(":common"))
     compileOnly("io.papermc.paper:paper-api:26.2.build.+")
 
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("")
+}
+
+tasks.jar {
+    enabled = false
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
