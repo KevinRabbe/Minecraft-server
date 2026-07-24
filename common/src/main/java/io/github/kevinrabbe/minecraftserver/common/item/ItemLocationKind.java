9f20cdff8445fc45c185d76de6b3800d16bb7898
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 04:17:10 +0200

    Add pending delivery item custody

--- common/src/main/java/io/github/kevinrabbe/minecraftserver/common/item/ItemLocationKind.java
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/item/ItemLocationKind.java
@@ -3,6 +3,7 @@ package io.github.kevinrabbe.minecraftserver.common.item;
 /** Current authoritative locations for individual items. Add new custody types only with their owning system. */
 public enum ItemLocationKind {
     PLAYER_INVENTORY,
+    PENDING_DELIVERY,
     QUARANTINE,
     DESTROYED
 }
