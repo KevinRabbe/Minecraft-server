5cd2f8b2fb7dbdbd55ca1397cea04cf0fe6df39b
KevinRabbe <rabbekevin@icloud.com>
2026-07-25 01:09:03 +0200

    Add clan role type

---
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/clan/ClanRole.java
@@ -0,0 +1,8 @@
+package io.github.kevinrabbe.minecraftserver.common.clan;
+
+/** Small stable launch role set; detailed capabilities remain separately configurable. */
+public enum ClanRole {
+    LEADER,
+    OFFICER,
+    MEMBER
+}
