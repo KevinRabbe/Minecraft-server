e88c3a6a0fc2eb02545000c9111e509a17d18e89
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 03:15:44 +0200

    Add strict JSON content parser

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation("com.zaxxer:HikariCP:7.1.0")
     implementation("org.flywaydb:flyway-core:12.10.0")
     implementation("org.flywaydb:flyway-database-postgresql:12.10.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.22.1")
     runtimeOnly("org.postgresql:postgresql:42.7.13")
 
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
