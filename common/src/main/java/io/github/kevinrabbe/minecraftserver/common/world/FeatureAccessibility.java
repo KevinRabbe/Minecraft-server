e78d01a59e4148972e1a87202d5531751a30abc4
KevinRabbe <rabbekevin@icloud.com>
2026-07-25 00:56:08 +0200

    Add feature accessibility type

---
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/world/FeatureAccessibility.java
@@ -0,0 +1,7 @@
+package io.github.kevinrabbe.minecraftserver.common.world;
+
+/** Persistent logical accessibility; separate from whether runtime infrastructure is currently active. */
+public enum FeatureAccessibility {
+    LOCKED,
+    AVAILABLE
+}
