f564852090ca44b339b0c624c294d1dfcd95db57
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 03:37:25 +0200

    Add fail-closed ItemStack identity exception

---
+++ paper/src/main/java/io/github/kevinrabbe/minecraftserver/paper/PaperItemRepresentationException.java
@@ -0,0 +1,11 @@
+package io.github.kevinrabbe.minecraftserver.paper;
+
+final class PaperItemRepresentationException extends RuntimeException {
+    PaperItemRepresentationException(String message) {
+        super(message);
+    }
+
+    PaperItemRepresentationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
