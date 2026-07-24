781934c3d5dee8dda26f5e150cfb82f2d860952b
KevinRabbe <rabbekevin@icloud.com>
2026-07-24 03:24:42 +0200

    Add authoritative unique item location kinds

---
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/item/ItemLocationKind.java
@@ -0,0 +1,8 @@
+package io.github.kevinrabbe.minecraftserver.common.item;
+
+/** Current authoritative locations for individual items. Add new custody types only with their owning system. */
+public enum ItemLocationKind {
+    PLAYER_INVENTORY,
+    QUARANTINE,
+    DESTROYED
+}
