dba482413a7673d53fc9fbc56e4ebfba549aec8b
KevinRabbe <rabbekevin@icloud.com>
2026-07-25 01:09:17 +0200

    Add clan war status type

---
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/pvp/ClanWarStatus.java
@@ -0,0 +1,11 @@
+package io.github.kevinrabbe.minecraftserver.common.pvp;
+
+public enum ClanWarStatus {
+    CHALLENGED,
+    ACCEPTED,
+    ROSTER_LOCKED,
+    ACTIVE,
+    COMPLETED,
+    CANCELLED,
+    FAILED
+}
