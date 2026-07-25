186b465d4c49c64a4ee4d4d69e5362cbe6aa24b6
KevinRabbe <rabbekevin@icloud.com>
2026-07-25 05:05:53 +0200

    Add clan asset exception

---
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/clan/ClanAssetException.java
@@ -0,0 +1,11 @@
+package io.github.kevinrabbe.minecraftserver.common.clan;
+
+public final class ClanAssetException extends RuntimeException {
+    public ClanAssetException(String message) {
+        super(message);
+    }
+
+    public ClanAssetException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
