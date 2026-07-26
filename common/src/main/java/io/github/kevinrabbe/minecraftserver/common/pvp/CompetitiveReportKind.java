c49907dfb98a459808b3195087507b3d55e2e83c
KevinRabbe <rabbekevin@icloud.com>
2026-07-26 13:18:02 +0200

    Add competitive report kind

---
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/pvp/CompetitiveReportKind.java
@@ -0,0 +1,7 @@
+package io.github.kevinrabbe.minecraftserver.common.pvp;
+
+/** Intentionally tiny outcome vocabulary exposed by the untrusted legacy runtime. */
+public enum CompetitiveReportKind {
+    WINNER,
+    FAILURE
+}
