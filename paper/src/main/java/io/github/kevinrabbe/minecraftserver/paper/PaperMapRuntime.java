32360026d72449648ba39a1639a106b10a0fbcef
KevinRabbe <rabbekevin@icloud.com>
2026-07-26 04:09:38 +0200

    Expose qualified Map leaderboard through Paper

--- paper/src/main/java/io/github/kevinrabbe/minecraftserver/paper/PaperMapRuntime.java
+++ paper/src/main/java/io/github/kevinrabbe/minecraftserver/paper/PaperMapRuntime.java
@@ -164,6 +164,7 @@ final class PaperMapRuntime {
             gameplay.start();
         }
 
+        PaperLeaderboardRouterCommand.scheduleInstall(plugin, dataSource);
         return new PaperMapRuntime(openService, encounterController, recoveryTask);
     }
 
