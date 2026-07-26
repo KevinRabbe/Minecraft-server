3c87306e2d6fa93f6d4494ae1087ddc6c3984d19
KevinRabbe <rabbekevin@icloud.com>
2026-07-26 13:17:13 +0200

    Add competitive execution value types

---
+++ common/src/main/java/io/github/kevinrabbe/minecraftserver/common/pvp/CompetitiveActivityKind.java
@@ -0,0 +1,7 @@
+package io.github.kevinrabbe.minecraftserver.common.pvp;
+
+/** Durable activity categories that may be executed by the isolated 1.8.9 runtime. */
+public enum CompetitiveActivityKind {
+    RANKED_ARENA,
+    CLAN_WAR
+}
