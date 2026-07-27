1f6392b79beaec5aa3a9579a762be77456f0e4c4
KevinRabbe <rabbekevin@icloud.com>
2026-07-27 07:38:08 +0200

    Test item use requirement presentation

--- paper/src/test/java/io/github/kevinrabbe/minecraftserver/paper/PaperItemRuntimePresentationTest.java
+++ paper/src/test/java/io/github/kevinrabbe/minecraftserver/paper/PaperItemRuntimePresentationTest.java
@@ -6,8 +6,11 @@ import io.github.kevinrabbe.minecraftserver.common.item.ItemIdentityKind;
 import io.github.kevinrabbe.minecraftserver.common.item.ItemLocation;
 import io.github.kevinrabbe.minecraftserver.common.item.ItemRollProfile;
 import io.github.kevinrabbe.minecraftserver.common.item.ItemRuntimeStatSnapshot;
+import io.github.kevinrabbe.minecraftserver.common.item.ItemSkillRequirement;
+import io.github.kevinrabbe.minecraftserver.common.item.ItemUseRequirements;
 import io.github.kevinrabbe.minecraftserver.common.item.RollRange;
 import io.github.kevinrabbe.minecraftserver.common.item.UpgradeState;
+import io.github.kevinrabbe.minecraftserver.common.progression.SkillId;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -19,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PaperItemRuntimePresentationTest {
     @Test
-    void equipmentShowsExactQualityCurrentIntrinsicMultiplierAndUpgradeSeparately() {
+    void equipmentShowsRollUpgradeAndStaticUseRequirementsSeparately() {
         ItemDefinition definition = new ItemDefinition(
                 "equipment.test_sword",
                 "IRON_SWORD",
@@ -27,7 +30,11 @@ class PaperItemRuntimePresentationTest {
                 1,
                 ItemCategory.EQUIPMENT,
                 ItemIdentityKind.INDIVIDUAL,
-                new ItemRollProfile(Map.of("damage", new RollRange(10_000, 12_000)))
+                new ItemRollProfile(Map.of("damage", new RollRange(10_000, 12_000))),
+                new ItemUseRequirements(List.of(
+                        new ItemSkillRequirement(new SkillId("combat"), 10),
+                        new ItemSkillRequirement(new SkillId("bounty.spider"), 5)
+                ))
         );
         ItemRuntimeStatSnapshot snapshot = snapshot(
                 definition.definitionId(),
@@ -39,9 +46,32 @@ class PaperItemRuntimePresentationTest {
         assertEquals(
                 List.of(
                         "damage roll: 50.00% quality (110.00% base)",
-                        "Upgrade: +3"
+                        "Upgrade: +3",
+                        "Requires bounty.spider level 5",
+                        "Requires combat level 10"
                 ),
                 PaperItemRuntimePresentation.describe(definition, snapshot)
+        );
+    }
+
+    @Test
+    void staticUseRequirementsAreStableAndDoNotNeedPlayerProgression() {
+        ItemDefinition definition = new ItemDefinition(
+                "equipment.requirement_test",
+                "IRON_SWORD",
+                "Requirement Test",
+                1,
+                ItemCategory.EQUIPMENT,
+                ItemIdentityKind.INDIVIDUAL,
+                ItemRollProfile.NONE,
+                new ItemUseRequirements(List.of(
+                        new ItemSkillRequirement(new SkillId("mining"), 12)
+                ))
+        );
+
+        assertEquals(
+                List.of("Requires mining level 12"),
+                PaperItemRuntimePresentation.describeUseRequirements(definition)
         );
     }
 
